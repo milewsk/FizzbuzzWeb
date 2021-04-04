094ceda4adb9491e4c3878fbb93c79f8e0c39c9a
Konrad Milewski <konradmilewski2000@interia.eu>
2021-04-04 18:45:32 +0200

    Zmiany: +Dodanie folderu data z "CalculateContext" + Dodanie potrzebnych pakietów

---
+++ Data/CalculateContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzbuzzWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FizzbuzzWeb.Data
+{
+    public class CalculateContext : DbContext
+    {
+        public DbSet<Calculate> Calculates { get; set; }
+
+    }
+}
